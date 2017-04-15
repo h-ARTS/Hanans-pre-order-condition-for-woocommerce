fb1d23f25fa3aae1783bf390980f6d657e5f0b86
Hanan Mufti <hananmufti@Hanans-MacBook-Pro.local>
2017-04-15 12:41:59 +0200

    First Commit

---
+++ class-pre-order-condition.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin Name:       Hanan's Pre-Order condition for WooCommerce
+ * Plugin URI:        http://github.com/tommcfarlin/post-meta-manager
+ * Description:       This PlugIn will help your customers to specify their pre-order.
+ * Version:           0.0.1
+ * Author:            Hanan Mufti
+ * Author URI:        N.A
+ * Text Domain:       hanans-pre-order-condition-woo
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ */
+
+ ?>
